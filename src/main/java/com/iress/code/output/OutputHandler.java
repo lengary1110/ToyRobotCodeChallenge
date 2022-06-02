b2114ef74b80b1991bf8ef0a6ea0e33dd61a209e
lengary1110 <ziyuanx1@student.unimelb.edu.au>
2022-06-02 21:10:21 +1000

    TR: modify OutputHandler.java

--- src/main/java/com/iress/code/output/OutputHandler.java
+++ src/main/java/com/iress/code/output/OutputHandler.java
@@ -8,9 +8,10 @@ import static com.iress.code.utils.ToyRobotConstants.OUTPUT_DESCRIPTION;
 @Slf4j
 public class OutputHandler {
     public void handle(Robot robot) {
-        StringBuilder outputStr = new StringBuilder();
-        outputStr.append(OUTPUT_DESCRIPTION)
-                .append(robot.getPosition().checkStatus());
+        String outputStr =
+                OUTPUT_DESCRIPTION +
+                robot.getPosition().checkStatus();
+        log.info(outputStr);
         System.out.println(outputStr);
     }
 }
